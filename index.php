723faa1a502b066f7e501a695ed80935618d8727
geragalanin <32804078+geragalanin@users.noreply.github.com>
2017-11-24 00:15:37 +0300

    g

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ $id = $output['message']['chat']['id'];
 $text = $output['message']['text'];
 $token = '490482772:AAHszXUudRHfOIh8306hne4EnQtlit5Kknw';
 
+$do = json_decode(file_get_contents("https://ggsite/ficom?pass=2299&uid=".$id."&what=1"),true);
+
 
 function SendMessage($token,$id,$message){
 	file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id."&text=".$message);
@@ -15,12 +17,18 @@ switch($text){
 	case '/start':
 		$message = 'Выберите, что Вам нужно: найти бота или зарегистрировать бота.';
 		SendMessage($token,$id,$message.KeyboardMenu1());
+		file_get_contents("https://ggsite/ficom?pass=2299&do=1&uid=".$id);
 	break;
 	case 'Поиск бота':
 		$message = '';
 		SendMessage($token,$id,$message);
 	break;
 	case 'Зарегистрировать бота':
+		$message = 'Отправьте ссылку на вашего бота в формате "@yourbot". (На данный момент один пользователь может зарегистрировать одного бота.)';
+		SendMessage($token,$id,$message);
+		file_get_contents("https://ggsite/ficom?pass=2299&do=1.2");
+	break;
+	case 'Редактировать бота':
 		$message = '';
 		SendMessage($token,$id,$message);
 	break;
@@ -31,7 +39,18 @@ switch($text){
 
 
 function KeyboardMenu1(){
-	$buttons = [['Поиск бота'],['Зарегистрировать бота']];
+	$buttons = [['Поиск бота'],['Зарегистрировать бота'],['Редактировать бота']];
+	$keyboard = json_encode($keyboard = ['keyboard' => $buttons,       //кнопки 
+										  'resize_keyboard' => true,   //размер кнопок норм, если true
+										  'one_time_keyboard' => true, //убирается сама, если true
+										  'selective' => false]);
+										  
+	$reply_markup = '&reply_markup='.$keyboard.'';
+	return $reply_markup; 
+}
+
+function KeyboardMenu2(){
+	$buttons = ['Отмена'];
 	$keyboard = json_encode($keyboard = ['keyboard' => $buttons,       //кнопки 
 										  'resize_keyboard' => true,   //размер кнопок норм, если true
 										  'one_time_keyboard' => true, //убирается сама, если true
