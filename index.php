bbba8926fb6ad956a254248acac2ca178a1290c5
geragalanin <32804078+geragalanin@users.noreply.github.com>
2018-01-12 18:07:58 +0300

    1

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $text = $output['message']['text'];
-if(stripos($text,'start=')===true){
+//if(stripos($text,'start=')===true){
 	$key = substr($text, 7, 0); ;
-}
+//}
 //$key = $_GET['start']; 
 //file_get_contents('http://ggsite.ru/ficom/index.php?id='.$id.'&text='.$text);
 
