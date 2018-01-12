c458859da52f81bd4dcb9b6d2fe81ba343caaaa8
geragalanin <32804078+geragalanin@users.noreply.github.com>
2018-01-12 18:40:45 +0300

    3

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $text = $output['message']['text'];
 
-if(strrpos($text,"start=") === true){
+if(stripos($text,"start=") === true){
 	$key = substr($text, 7);
 }
 
