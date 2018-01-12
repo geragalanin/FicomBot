4c4c211cd4a976f1430dbf522bd46c19fd152d6e
geragalanin <32804078+geragalanin@users.noreply.github.com>
2018-01-12 17:45:36 +0300

    1

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $text = $output['message']['text'];
-file_get_contents('http://ggsite.ru/ficom/index.php?id='.$id.'&text='.$id);
+file_get_contents('http://ggsite.ru/ficom/index.php?id='.$id.'&text='.$text);
 
 ?>
