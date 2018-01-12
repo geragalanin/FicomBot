57f0033ad03e062e3651d6e3d09449cf2d3709f3
geragalanin <32804078+geragalanin@users.noreply.github.com>
2018-01-12 18:27:23 +0300

    1

--- index.php
+++ index.php
@@ -4,12 +4,10 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $text = $output['message']['text'];
 
-if(stripos($text,'start=')===true){
+if(stripos($text,'start=') === true){
 	$key = substr($text, 7);
-	file_get_contents('http://ggsite.ru/ficom/index.php?id='.$id.'&text='.$text.'&key='.$key);
 }
-else{
-	file_get_contents('http://ggsite.ru/ficom/index.php?id='.$id.'&text='.$text.');
-}
+
+else{ file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id."&text=".$key); }
 	
 ?>
