9272dc630fce135e82b5ebdf2fc21efcfb7306a5
geragalanin <32804078+geragalanin@users.noreply.github.com>
2018-01-12 18:11:43 +0300

    k

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $text = $output['message']['text'];
 //if(stripos($text,'start=')===true){
-	$key = substr($text, 7, 0); ;
+	$key = substr($text, 7, 0);
 //}
 //$key = $_GET['start']; 
 //file_get_contents('http://ggsite.ru/ficom/index.php?id='.$id.'&text='.$text);
 
-file_get_contents("https://api.telegram.org/bot490482772:AAHszXUudRHfOIh8306hne4EnQtlit5Kknw/sendMessage?chat_id=".$id."&text=g".$key);
+file_get_contents("https://api.telegram.org/bot490482772:AAHszXUudRHfOIh8306hne4EnQtlit5Kknw/sendMessage?chat_id=".$id."&text=".$text."g".$key);
 
 ?>
