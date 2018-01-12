5b225a96452c121c60973ef610f4cd68c0d920ad
geragalanin <32804078+geragalanin@users.noreply.github.com>
2018-01-12 16:42:06 +0300

    1

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 ﻿<?php
 
 $output = file_get_contents('php://input');
-//header('Location: http://ggsite.ru/ficom/index.php?out='.$output);
 file_get_contents('http://ggsite.ru/ficom/index.php?out='.$output);
 	
 ?>
