e9c10503d494c8b537b70fe5c024829aaf02d3b1
geragalanin <32804078+geragalanin@users.noreply.github.com>
2018-01-12 18:38:06 +0300

    1

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $text = $output['message']['text'];
 
-if(stristr($text,"start=") === true){
+//if(stristr($text,"start=") === true){
 	$key = substr($text, 7);
-}
+//}
 
 file_get_contents("https://api.telegram.org/bot490482772:AAHszXUudRHfOIh8306hne4EnQtlit5Kknw/sendMessage?chat_id=".$id."&text=".$key); 
 	
